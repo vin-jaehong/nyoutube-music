ba128e00194b7943bb65a09c1c82c041737b715e
JaeHong <dnflwoghddl@gmail.com>
2024-06-02 23:22:21 +0900

    에러 수정

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -2,9 +2,18 @@
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { useEffect, useState } from 'react';
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return !isMounted ? null : (
+    <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  );
 };
 
 export default ThemeProvider;
