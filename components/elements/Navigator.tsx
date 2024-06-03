6b513b8bf019ded2dc564c7a9f0e3431d964d60a
JaeHong <dnflwoghddl@gmail.com>
2024-06-03 23:31:37 +0900

    navigation component 작업

--- components/elements/Navigator.tsx
+++ components/elements/Navigator.tsx
@@ -1,5 +1,71 @@
+'use client';
+
+import { GoHome } from 'react-icons/go';
+import { FiPlus, FiMusic, FiCompass } from 'react-icons/fi';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+
 const Navigator = () => {
-  return <div>Navigator</div>;
+  const pathname = usePathname();
+
+  const routes = useMemo(() => {
+    return [
+      {
+        id: 0,
+        icon: <GoHome size={24} />,
+        label: '홈',
+        isActive: pathname === '/',
+        href: '/',
+      },
+      {
+        id: 1,
+        icon: <FiCompass size={24} />,
+        label: '둘러보기',
+        isActive: pathname === '/explore',
+        href: '/explore',
+      },
+      {
+        id: 2,
+        icon: <FiMusic size={20} />,
+        label: '보관함',
+        isActive: pathname === '/library',
+        href: '/library',
+      },
+    ];
+  }, [pathname]);
+
+  return (
+    <div>
+      <section className="flex flex-col gap-2 p-4">
+        {routes.map((route) => {
+          return (
+            <Link href={route.href} key={`nav-list-item-${route.id}`}>
+              <div
+                className={cn(
+                  'flex flex-row items-center gap-4 text-[16px] hover:bg-neutral-700 rounded-lg p-2',
+                  route.isActive && 'bg-neutral-800'
+                )}
+              >
+                {route.icon}
+                <span>{route.label}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+      <section className="px-6">
+        <div className="w-full h-[1px] bg-neutral-700"></div>
+      </section>
+      <section className="px-6">
+        <div className="flex flex-row items-center bg-neutral-700 my-6 rounded-3xl p-2 font-[200] justify-center gap-2 hover:bg-neutral-600 cursor-pointer">
+          <FiPlus size={24} />
+          <span>새 재생목록</span>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Navigator;
