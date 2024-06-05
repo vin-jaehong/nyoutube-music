1021d058add522b7168cf7824dabfa52003033b9
JaeHong <dnflwoghddl@gmail.com>
2024-06-05 22:12:52 +0900

    header 컴포넌트 추가 및 퍼블리싱 작업

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,11 @@
-const layout = ({ children }: any) => {
-  return <div>{children}</div>;
+import Header from '@/components/header';
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full h-full">
+      <Header>{children}</Header>
+    </div>
+  );
 };
 
 export default layout;
---
+++ components/header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+const Header = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <header className="relative overflow-y-auto w-full h-full">
+      <section className="absolute top-0 w-full">
+        <div className="relative h-[400px] w-full">
+          <Image
+            fill
+            src="https://images.unsplash.com/photo-1707833558984-3293e794031c"
+            alt="mediaItem"
+            className="object-cover"
+          />
+          <div className="absolute top-0 bg-black opacity-40 w-full h-[400px]"></div>
+          <div className="absolute top-0 bg-gradient-to-t from-black w-full h-[400px]"></div>
+        </div>
+      </section>
+      <section className="absolute">{children}</section>
+    </header>
+  );
+};
+
+export default Header;
